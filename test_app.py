536b5a3b0e35919d7c58a24b70dcf55cec16a6bd
Rithish Kumar V <rithishvellingiri@gmail.com>
2026-03-26 09:50:23 +0530

    2 files added

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import add, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_fail_case():
+    assert add(2, 2) == 5   #for demo
